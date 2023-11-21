7bdf42c0ce1f2df7f1af86e74a2dc04f587bdb17
Dilsher <dilsherb@gmail.com>
2023-11-21 14:53:06 +0300

    random color generator app's basic setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RandomColor from './projects/RandomColor/RandomColor'
+
+function App() {
+
+  return (
+    <>
+      <RandomColor />
+    </>
+  )
+}
+
+export default App
